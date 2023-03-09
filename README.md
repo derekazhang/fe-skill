2ac15d04c4b311df3c84faabce80b85e038ff025
aimin <dami7786@qq.com>
2023-03-09 18:58:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=derekazhang/fe-skill&type=Date)](https://star-history.com/#derekazhang/fe-skill&Date)
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -25,30 +25,32 @@ module.exports = {
         initialOpenGroupIndex: -1, // 可选的, 默认值是 0
       },
     ],
+
+    lastUpdated: "最近更新",
+    // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
+    nextLinks: false,
+    // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
+    prevLinks: false,
+
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: "derekazhang/fe-skill",
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: "查看源码",
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: "vuejs/vuepress",
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: "docs",
+    // 假如文档放在一个特定的分支下：
+    docsBranch: "master",
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: "帮助我们改善此页面！",
+
+    smoothScroll: true,
   },
-
-  lastUpdated: "最近更新",
-  // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
-  nextLinks: false,
-  // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
-  prevLinks: false,
-
-  // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-  repo: "vuejs/vuepress",
-  // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-  // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-  repoLabel: "查看源码",
-
-  // 以下为可选的编辑链接选项
-
-  // 假如你的文档仓库和项目本身不在一个仓库：
-  docsRepo: "vuejs/vuepress",
-  // 假如文档不是放在仓库的根目录下：
-  docsDir: "docs",
-  // 假如文档放在一个特定的分支下：
-  docsBranch: "master",
-  // 默认是 false, 设置为 true 来启用
-  editLinks: true,
-  // 默认为 "Edit this page"
-  editLinkText: "帮助我们改善此页面！",
 };
